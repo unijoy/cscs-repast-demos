c9a19d618e01ab665aee5b239e08795ea7a10f39
richard.legendi@gmail.com <richard.legendi@gmail.com@cbadf72f-fc06-7a9d-c51c-680f2894948d>
2011-06-17 15:12:58 +0000

    [AMEND] Forgot to add Predator class.

---
+++ richard/StupidModel/trunk/src/stupidmodel/agents/Predator.java
@@ -0,0 +1,147 @@
+/*
+ * Version info:
+ *     $HeadURL$
+ *     $LastChangedDate$
+ *     $LastChangedRevision$
+ *     $LastChangedBy$
+ */
+package stupidmodel.agents;
+
+import java.util.List;
+
+import repast.simphony.engine.schedule.ScheduledMethod;
+import repast.simphony.query.space.grid.GridCell;
+import repast.simphony.query.space.grid.GridCellNgh;
+import repast.simphony.random.RandomHelper;
+import repast.simphony.space.grid.Grid;
+import repast.simphony.space.grid.GridPoint;
+import repast.simphony.util.ContextUtils;
+import repast.simphony.util.SimUtilities;
+import stupidmodel.common.Constants;
+import stupidmodel.common.SMUtils;
+
+/**
+ * @author Richard O. Legendi (richard.legendi)
+ * @since 2.0-beta, 2011
+ * @since Model 16
+ * @version $Id$
+ */
+public class Predator {
+	public Predator() {
+		super();
+	}
+
+	/**
+	 * Scheduled after all the {@link Bug} actions.
+	 */
+	@ScheduledMethod(start = 1, interval = 1, priority = 1)
+	public void hunt() {
+		// Reference for the used grid
+		final Grid<Object> grid = getGrid();
+		// Get the grid location of this Bug
+		final GridPoint location = grid.getLocation(this);
+
+		// TODO Make constants
+
+		// First, a predator looks through a shuffled list of its immediately
+		// neighboring cells (including its own cell).
+		// TODO Test if 1 neighbourhood not 0
+		final List<GridCell<HabitatCell>> cellNeighborhood = new GridCellNgh<HabitatCell>(
+				grid, location, HabitatCell.class, 1, 1).getNeighborhood(true);
+
+		SimUtilities.shuffle(cellNeighborhood, RandomHelper.getUniform());
+
+		// As soon as the predator finds a bug in one of these cells it "kills"
+		// the bug and moves into the cell.
+
+		for (final GridCell<HabitatCell> cell : cellNeighborhood) {
+			if (hasBug(grid, cell)) {
+				// (However, if the cell already contains a predator, the
+				// hunting predator simply quits and remains at its current
+				// location.)
+				if (hasPredator(grid, cell)) {
+					return;
+				}
+
+				killBugAt(grid, cell);
+				moveTo(grid, cell);
+				return;
+			}
+		}
+
+		// If these cells contain no bugs, the predator moves randomly to one of
+		// them.
+		final GridCell<HabitatCell> randomCell = SMUtils
+				.randomElementOf(cellNeighborhood);
+		moveTo(grid, randomCell);
+	}
+
+	// TODO Merge the next 2 functions, a type parameter T and a class may solve
+	// it
+	private boolean hasBug(final Grid<Object> grid,
+			final GridCell<HabitatCell> cell) {
+		assert (grid != null);
+		assert (cell != null);
+
+		final int predatorCount = new GridCellNgh<Bug>(grid, cell.getPoint(),
+				Bug.class, 0, 0).getNeighborhood(true).size();
+
+		return (predatorCount > 0);
+	}
+
+	private boolean hasPredator(final Grid<Object> grid,
+			final GridCell<HabitatCell> cell) {
+		assert (grid != null);
+		assert (cell != null);
+
+		final int predatorCount = new GridCellNgh<Predator>(grid,
+				cell.getPoint(), Predator.class, 0, 0).getNeighborhood(true)
+				.size();
+
+		return (predatorCount > 0);
+	}
+
+	private void killBugAt(final Grid<Object> grid,
+			final GridCell<HabitatCell> cell) {
+		assert (grid != null);
+		assert (cell != null);
+		assert (hasBug(grid, cell));
+
+		final List<GridCell<Bug>> bugsAt = new GridCellNgh<Bug>(grid,
+				cell.getPoint(), Bug.class, 0, 0).getNeighborhood(true);
+
+		assert (1 == bugsAt.size());
+		bugsAt.get(0).items().iterator().next().die();
+	}
+
+	private void moveTo(final Grid<Object> grid,
+			final GridCell<HabitatCell> cell) {
+		assert (grid != null);
+		assert (cell != null);
+
+		// We have our new GridPoint to move to, so relocate agent
+		final GridPoint newGridPoint = cell.getPoint();
+		grid.moveTo(this, newGridPoint.getX(), newGridPoint.getY());
+	}
+
+	/**
+	 * Returns a reference to the grid on which the agent is located at.
+	 * 
+	 * @return the <code>Grid</code> on which the agent is located; <i>cannot be
+	 *         <code>null</code></i>
+	 * @since Model 2
+	 */
+	// TODO Generalize this
+	protected Grid<Object> getGrid() {
+		@SuppressWarnings("unchecked")
+		final Grid<Object> grid = (Grid<Object>) ContextUtils.getContext(this)
+				.getProjection(Constants.GRID_ID);
+
+		if (null == grid) {
+			throw new IllegalStateException("Cannot locate grid in context.");
+		}
+
+		return grid;
+	}
+
+}
