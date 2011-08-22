afb3e7e4d24b03b96981106b84b635afa65cd2f5
richard.legendi@gmail.com <richard.legendi@gmail.com@cbadf72f-fc06-7a9d-c51c-680f2894948d>
2011-08-22 08:35:38 +0000

    Backup note.

--- richard/ElFarol/trunk/src/elfarol/strategies/AStrategy.java
+++ richard/ElFarol/trunk/src/elfarol/strategies/AStrategy.java
@@ -12,9 +12,14 @@ import static elfarol.ParameterWrapper.getMemorySize;
 import java.util.Arrays;
 
 /**
+ * In the current implementation of the model, each agent has a set of randomly
+ * initialized autoregressive models (sequences of AR coefficients) that are not
+ * fitted.
+ * 
  * @author Richard O. Legendi (richard.legendi)
  * @since 2.0-beta, 2011
- * @version $Id$
+ * @version $Id: AStrategy.java 514 2011-07-08 14:50:51Z
+ *          richard.legendi@gmail.com $
  */
 public abstract class AStrategy {
 	protected final double[] weights;
